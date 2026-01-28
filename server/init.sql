2cde7e43f86779a800c3213a494a75d643356b46
MamounKolovos <mamoun.kolovos@gmail.com>
2026-01-28 02:53:42 -0500

    feat(db): make emails unique and case insensitive. add update trigger to users. updated_at

--- server/init.sql
+++ server/init.sql
@@ -1,8 +1,23 @@
-create table users (
-  id integer generated always as identity primary key,
-  email text not null,
-  password_hash text not null,
-  name text not null,
-  created_at timestamp not null default now(),
-  updated_at timestamp not null default now()
-)
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE users (
+  id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email citext NOT NULL UNIQUE,
+  password_hash text NOT NULL,
+  name text NOT NULL,
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  updated_at timestamp NOT NULL DEFAULT NOW()
+);
+
+create or replace function update_updated_at()
+  returns trigger as $$
+begin
+  new.updated_at = now() at time zone 'utc';
+  return new;
+end
+$$ language plpgsql;
+
+create trigger users_updated_at
+  before update on users
+  for each row
+  execute procedure update_updated_at();
